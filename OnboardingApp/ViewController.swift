1ab97861d6c6eb650e2a2eae3b78284d5e6be5d8
shotaro.doi <shotaro.doi@dena.com>
2024-01-31 16:27:43 +0900

    Initial Commit

---
+++ OnboardingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingApp
+//
+//  Created by Shotaro Doi on 2024/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
